dcdce05774559acc3ed478d2c4175a5f0801b056
meldv <maaret.dufva@gmail.com>
2016-09-17 18:40:38 +0300

    kooditiedostoja ja ohjelman rakennetta

---
+++ typetutor/src/main/java/fi/maaretdufva/gui/Main.java
@@ -0,0 +1,12 @@
+/*
+ *  This is the main class of TypeTutor
+ */
+package fi.maaretdufva.gui;
+
+/**
+ *
+ * @author Maaret Dufva
+ */
+public class Main {
+    
+}
---
+++ typetutor/src/main/java/fi/maaretdufva/users/User.java
@@ -0,0 +1,12 @@
+/*
+ * This is the main class for users.
+ */
+package fi.maaretdufva.users;
+
+/**
+ *
+ * @author Maaret Dufva
+ */
+public class User {
+    
+}
