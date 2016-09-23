f1a3ef594f5aa6924b938f6b8265ca4d46673748
MelDv <maaret.dufva@gmail.com>
2016-09-23 23:48:32 +0300

    käyttöliittymän rakentelua

--- typetutor/src/main/java/fi/maaretdufva/gui/Gui.java
+++ typetutor/src/main/java/fi/maaretdufva/gui/Gui.java
@@ -5,9 +5,19 @@
  */
 package fi.maaretdufva.gui;
 
+import java.awt.ComponentOrientation;
 import java.awt.Container;
 import java.awt.Dimension;
+import java.awt.GridBagConstraints;
+import static java.awt.GridBagConstraints.RELATIVE;
+import java.awt.GridBagLayout;
+import java.awt.Insets;
+import java.awt.LayoutManager;
+import javax.swing.JButton;
 import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JPanel;
+import javax.swing.JTextField;
 import javax.swing.WindowConstants;
 
 /**
@@ -29,13 +39,48 @@ public class Gui implements Runnable {
         frame.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
 
         createComponents(frame.getContentPane());
-        
+
         frame.pack();
         frame.setVisible(true);
     }
 
-    private void createComponents(Container container) {
+    private void createComponents(Container pane) {
+        JButton button;
+        pane.setLayout(new GridBagLayout());
+        GridBagConstraints c = new GridBagConstraints();
+        c.fill = GridBagConstraints.HORIZONTAL;
 
+        button = new JButton("Rekisteröidy");
+        c.weightx = 0.1;
+        c.gridx = 0;
+        c.gridy = 0;
+        pane.add(button, c);
+
+        button = new JButton("Kirjaudu");
+        c.gridx = 1;
+        c.gridy = 0;
+        pane.add(button, c);
+
+        button = new JButton("Kokeile kirjautumatta");
+        c.gridx = 3;
+        c.gridy = 0;
+        pane.add(button, c);
+
+        JLabel basicGuide = new JLabel("Kirjoita ruutuun tuleva teksti kymmensormijärjestelmällä.");
+        c.gridx = 0;
+        c.gridy = 1;
+        pane.add(basicGuide, c);
+
+        JLabel text = new JLabel("Kirjoita tämä");
+        c.gridx = 0;
+        c.gridy = 5;
+        pane.add(text, c);
+
+        JTextField writeThis = new JTextField("Kirjoita tähän");
+        c.gridx = 0;
+        c.gridy = 6;
+        writeThis.setEditable(true);
+        pane.add(writeThis, c);
     }
 
     public JFrame getFrame() {
--- typetutor/src/main/java/fi/maaretdufva/gui/Main.java
+++ typetutor/src/main/java/fi/maaretdufva/gui/Main.java
@@ -14,6 +14,7 @@ import javax.swing.SwingUtilities;
 public class Main {
 
     public static void main(String[] args) {
+        
         Gui gui = new Gui();
         SwingUtilities.invokeLater(gui);
 //        Account tili = new Account();       
